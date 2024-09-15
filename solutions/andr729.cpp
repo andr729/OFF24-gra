ae950a670ab52b725e56fb8ffe65872268935250
Andrzej <andrzej@rpi.pl>
2024-09-15 18:46:49 +0200

    more stuff

--- solutions/andr729.cpp
+++ solutions/andr729.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <array>
 
 using u64 = uint64_t;
+using i64 = int64_t;
 
 // constexpr u64 N = 15;
 // constexpr u64 M = 20;
@@ -20,11 +22,37 @@ constinit bool global_params_set = false;
 
 /*******************/
 
-
-struct Pos {
-	u64 x = 0;
-	u64 y = 0;
+struct Vec {
+	i64 x = 0;
+	i64 y = 0;
 };
+
+enum class Direction {
+	UP    = 0,
+	DOWN  = 1,
+	LEFT  = 2,
+	RIGHT = 3,
+};
+
+constexpr std::array<Direction, 4> DirectionArr = {
+	Direction::UP,
+	Direction::DOWN,
+	Direction::LEFT,
+	Direction::RIGHT
+};
+
+Vec dirToVec(Direction dir) {
+	switch (dir) {
+		case Direction::UP:
+			return {0, -1};
+		case Direction::DOWN:
+			return {0, 1};
+		case Direction::LEFT:
+			return {-1, 0};
+		case Direction::RIGHT:
+			return {1, 0};
+	}
+}
 
 template <typename T>
 struct QuadDirStorage {
@@ -42,12 +70,96 @@ struct QuadDirStorage {
 	T& right() {
 		return vals[3];
 	}
+
+	T& get(Direction dir) {
+		switch (dir) {
+			case Direction::UP:
+				return up();
+			case Direction::DOWN:
+				return down();
+			case Direction::LEFT:
+				return left();
+			case Direction::RIGHT:
+				return right();
+		}
+	}
 };
+
 
 void skipNewLine() {
 	char c;
 	std::cin >> std::noskipws >> c;
 	assert(c == '\n');
+}
+
+
+// cause std...
+struct Bool {
+	bool b;
+};
+
+struct BoolLayer {
+private:
+	std::vector<Bool> bullets;
+	bool& getRef(u64 x, u64 y) {
+		return this->bullets.at(x * m + y).b;
+	}
+public:
+	BoolLayer(): bullets(n * m, {false}) {}
+
+	bool get(Vec pos) {
+		return getRef(pos.x, pos.y);
+	}
+
+	void set(Vec pos, bool val) {
+		getRef(pos.x, pos.y) = val;
+	}
+};
+
+struct BulletLayer {
+private:
+	QuadDirStorage<BoolLayer> bullets;
+
+public:
+	// BulletLayer() {}
+	// BulletLayer(const BulletLayer& other) = default;
+	// BulletLayer(BulletLayer&& other) = default;
+
+	// BulletLayer& operator=(BulletLayer&& other) = default;
+
+	void addBullet(Vec pos, Direction dir) {
+		bullets.get(dir).set(pos, true);
+	}
+
+	bool isBulletAt(Vec pos) {
+		bool res = false;
+		for (auto dir: DirectionArr) {
+			res |= bullets.get(dir).get(pos);
+		}
+		return res;
+	}
+
+	void moveBulletsWithWalls(Vec direction, const BoolLayer& walls ) {
+		// ideally we want to do it inplace for performance..
+		// for now we ignore it
+		BulletLayer new_bullets;
+
+		for (auto dir: DirectionArr) {
+
+		}
+
+
+		// @TODO: make sure it actually moves:
+		*this = std::move(new_bullets);
+	}
+};
+
+struct GameState {
+
+};
+
+namespace alpha_beta {
+	// TODO
 }
 
 
@@ -62,12 +174,12 @@ int main() {
 		global_params_set = true;
 	}
 
-	QuadDirStorage<std::vector<Pos>> bullets;
+	QuadDirStorage<std::vector<Vec>> bullets;
 	
-	std::vector<Pos> walls;
+	std::vector<Vec> walls;
 
-	Pos red_player;
-	Pos blue_player;
+	Vec red_player;
+	Vec blue_player;
 
 	for (u64 i = 0; i < n; i++) {
 		for (u64 j = 0; j < m; j++) {
