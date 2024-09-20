b5ae4d3a45f767659508752edbc1f300ff627916
Andrzej <andrzej@rpi.pl>
2024-09-20 11:22:34 +0200

    delete wiki snippet

--- solutions/andr729.cpp
+++ solutions/andr729.cpp
@@ -8,7 +8,7 @@ using u64 = uint64_t;
 using i64 = int64_t;
 
 // @TODO: make it bigger when cutoffs are efficient
-constexpr u64 AB_DEPTH = 3;
+constexpr u64 AB_DEPTH = 4;
 
 // constexpr u64 N = 15;
 // constexpr u64 M = 20;
@@ -568,27 +568,6 @@ struct ABGameState {
 
 
 namespace alpha_beta {
-	// PSUEDO CODE from https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning:
-	// function alphabeta(node, depth, α, β, maximizingPlayer) is
-    // if depth == 0 or node is terminal then
-    //     return the heuristic value of node
-    // if maximizingPlayer then
-    //     value := −∞
-    //     for each child of node do
-    //         value := max(value, alphabeta(child, depth − 1, α, β, FALSE))
-    //         if value > β then
-    //             break (* β cutoff *)
-    //         α := max(α, value)
-    //     return value
-    // else
-    //     value := +∞
-    //     for each child of node do
-    //         value := min(value, alphabeta(child, depth − 1, α, β, TRUE))
-    //         if value < α then
-    //             break (* α cutoff *)
-    //         β := min(β, value)
-    //     return value
-
 	template<bool INITIAL>
 	struct ABRetType {
 		using type = PositionEvaluation;
